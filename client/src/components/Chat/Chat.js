abbcb725aa02d4433f970231ab1893df802da56d
Jean Menezes <jeanmenezes.nave@gmail.com>
2020-10-22 02:04:56 -0300

    Fix miscomunication between server and client

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
+import queryString from "query-string";
 import io from "socket.io-client";
-import { Redirect } from 'react-router';
+import { Redirect } from "react-router";
 
-import TextContainer from '../TextContainer/TextContainer';
-import Messages from '../Messages/Messages';
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
 
-import './Chat.css';
+import "./Chat.css";
 
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [flag, setFlag]=useState(0);
-  const ENDPOINT = 'https://localhost:3000/';
+  const [flag, setFlag] = useState(0);
+  const ENDPOINT = "http://localhost:5000/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -27,52 +27,53 @@ const Chat = ({ location }) => {
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name)
+    setName(name);
 
-    socket.emit('join', { name, room }, (error) => {
-      console.log("trying to join")
-      if(error) { 
-        console.log(error)
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        console.log(error);
         setFlag(1);
         alert(error);
       }
     });
   }, [ENDPOINT, location.search]);
-  
+
   useEffect(() => {
-    socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
     });
-    
+
     socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-}, []);
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if(message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
     }
-  }
+  };
 
-    if (flag){
-      return (
-        <Redirect to="/" />
-      )
-    }
+  if (flag) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
-      <TextContainer users={users}/>
+      <TextContainer users={users} />
     </div>
   );
-}
+};
 
 export default Chat;
--- server/router.js
+++ server/router.js
@@ -1,9 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
-const express=require('express');
-const router=express.Router();
-
-router.get('/', (req, res)=> {
-    res.redirect('https://http://localhost:3000/');
-})
+router.get("/", (req, res) => {
+  res.redirect("http://localhost:3000/");
+});
 
 module.exports = router;
